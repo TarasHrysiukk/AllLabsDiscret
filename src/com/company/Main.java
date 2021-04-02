2a78a5cba1f7936e7f1bb771c270a01101db41a4
TarasHrysiukk <t.grusyk@gmail.com>
2021-04-02 19:28:59 +0300

    видалив зайве

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,23 +11,3 @@ public class Main {
        fileWorker.printArrayInt(array);
     }
 }
-//метод зчитування файлу таке саме в класі файлВоркер
-       /* try {
-            File file = new File("D:\\NULP\\DiscretMoelLabs\\Lab1Prym\\lab1.txt");
-            //создаем объект FileReader для объекта File
-            FileReader fr = new FileReader(file);
-            //создаем BufferedReader с существующего FileReader для построчного считывания
-            BufferedReader reader = new BufferedReader(fr);
-            // считаем сначала первую строку
-            String line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                // считываем остальные строки в цикле
-                line = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-//кінець методі зчитування файлу
