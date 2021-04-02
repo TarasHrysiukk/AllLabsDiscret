cfb460f3e90a1754eff50f38411cc7cb22fbb1b3
TarasHrysiukk <t.grusyk@gmail.com>
2021-04-02 21:03:58 +0300

    додав новий клас із методом прима, переніс у мейн медот виклик цього методі, та замінив локальний масивом даних  які зчитуються з файлу

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileWorker fileWorker = new FileWorker();
         PrymMethod prym = new PrymMethod();
-
+        System.out.println("зчитаний файл_________________________");
         int [][] array = fileWorker.FileWorkerMethod();
-        System.out.println("зчитаний файл вгорі___________ масив з файлу внизу");
+        System.out.println("масив з файлу_________________________");
        fileWorker.printArrayInt(array);
-        System.out.println("___________Алгоритм Прима___________");
+        System.out.println("Алгоритм Прима_________________________");
        prym.primMST(array);
     }
 }
